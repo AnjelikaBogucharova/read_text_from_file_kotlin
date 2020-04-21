75521434435c9b84bdee2f25f3bd4c5b033c064c
AnjelikaBogucharova <lika.bogucharova@mail.ru>
2020-04-21 23:17:30 +0300

    Чтение содержимого файла на Kotlin

--- src/main/kotlin/readerText/readFile.kt
+++ src/main/kotlin/readerText/readFile.kt
@@ -5,10 +5,10 @@ import java.nio.charset.Charset
 
 fun main() {
 
-    println("Читаем содержимое первого файла: simpleText.txt\r\n------------------------")
+    println("Читаем содержимое первого файла: simpleText.txt\r\n------------------------_")
     println(File("src/main/resources/simpleText.txt").readText(Charset.forName("UTF-8")))
 
-    println("\r\nЧитаем содержимое второого файла: oneMoreText.txt\r\n------------------------")
+    println("\r\nЧитаем содержимое второого файла: oneMoreText.txt\r\n-----------------------_-")
     println(File("src/main/resources/oneMoreText.txt").readText(Charset.forName("Windows-1251")))
 }
 
